2242f1f70fc0d34ffba568f1b8f61b7224c2c3ec
SharkmanTR <91312464+SharkmanTR@users.noreply.github.com>
2021-10-11 02:44:05 +0300

    Update girisyap.js

--- pages/girisyap.js
+++ pages/girisyap.js
@@ -30,7 +30,7 @@ export default function GirisYap(ls){
                         Giriş Yap
                     </button>
                     <div className={styles.foot}>
-                        <p className={styles.a1} onClick={window.location.href='/uyeol'}>Üye Ol</p>
+                        <p className={styles.a1}>Üye Ol</p>
                         <p className={styles.a2}>Şifremi Unuttum</p>
                     </div>
                 </div>
